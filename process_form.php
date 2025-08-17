f21bacd281140bf9a5492513210fc546f29a5760
John Morris <70734743+jmorris335@users.noreply.github.com>
2025-08-17 15:01:42 -0400

    Added print messages

--- process_form.php
+++ process_form.php
@@ -1,14 +1,14 @@
 <?php
 include ("functions.php");
 
-// if (isset($_POST['params'])) {
-//     $params = $_POST['params'];
-//     $group = $_POST['group_id'];
-//     $stage = $_POST['stage'];
-//     printf("<p>".$params."</p>");
-//     printf("<p>".$group."</p>");
-//     printf("<p>".$stage."</p>");
-// }
+if (isset($_POST['params'])) {
+    $params = $_POST['params'];
+    $group = $_POST['group_id'];
+    $stage = $_POST['stage'];
+    printf("<p>".$params."</p>");
+    printf("<p>".$group."</p>");
+    printf("<p>".$stage."</p>");
+}
 
 $conn = connectToServer(to_print: true);
 
@@ -17,5 +17,6 @@ $params_array = explode(",",$params);
 $db_values = array($group, $stage);
 $db_values = array_merge(array($group, $stage), $params_array);
 $query = insertParams("configs", $col_names, array($db_values));
+printf("<p>".$query."</p>");
 $conn->query($query);
 ?>
